a581378fca12b707f55496dee3809d8831397a10
schvbertt <diamenttx@gmail.com>
2020-05-27 02:25:10 +0200

    Updates

--- precache-manifest.0966ff48fd233de9d8539921ca6247c9.js
+++ precache-manifest.0966ff48fd233de9d8539921ca6247c9.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "40f6ee336c8fe88ce464861f73a1e8a9",
+    "revision": "f4b3aa79518211421cb2a1a409770972",
     "url": "/epic-bike-react/index.html"
   },
   {
-    "revision": "ddc2863ad85e83ae8c35",
+    "revision": "8593b4a661477591a4d1",
     "url": "/epic-bike-react/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "ad1152c08ed0b2152b3c",
-    "url": "/epic-bike-react/static/css/main.8e395ef2.chunk.css"
+    "revision": "af2b02409bf95a929dfb",
+    "url": "/epic-bike-react/static/css/main.69a61daf.chunk.css"
   },
   {
-    "revision": "ddc2863ad85e83ae8c35",
-    "url": "/epic-bike-react/static/js/2.7cfa83b0.chunk.js"
+    "revision": "8593b4a661477591a4d1",
+    "url": "/epic-bike-react/static/js/2.acdebc64.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/epic-bike-react/static/js/2.7cfa83b0.chunk.js.LICENSE.txt"
+    "url": "/epic-bike-react/static/js/2.acdebc64.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ad1152c08ed0b2152b3c",
-    "url": "/epic-bike-react/static/js/main.b3acae45.chunk.js"
+    "revision": "af2b02409bf95a929dfb",
+    "url": "/epic-bike-react/static/js/main.f67a75c4.chunk.js"
   },
   {
     "revision": "9077cb198108d6f0a55b",
@@ -36,12 +36,20 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/epic-bike-react/static/media/Woodplank-JRlE.22e1d335.ttf"
   },
   {
-    "revision": "17418a2a6786a32690aa84863cd00682",
-    "url": "/epic-bike-react/static/media/homepage.17418a2a.png"
+    "revision": "e84f4f940936e5a283953f19c6119cec",
+    "url": "/epic-bike-react/static/media/bike-service.e84f4f94.jpg"
+  },
+  {
+    "revision": "c575f24d4d69786a83ff50c724f0e38e",
+    "url": "/epic-bike-react/static/media/homepage.c575f24d.png"
   },
   {
     "revision": "937b6430b43ef1883c40a5a421da5f56",
     "url": "/epic-bike-react/static/media/logo.937b6430.png"
+  },
+  {
+    "revision": "d0bae3f412cbc02d49ecbda6c106aa45",
+    "url": "/epic-bike-react/static/media/sus-service.d0bae3f4.jpg"
   },
   {
     "revision": "4d28bc1456a55cd2690730efeca848c8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/epic-bike-react/precache-manifest.6b8a1912e8515fe9fcdf451bef6ad2c8.js"
+  "/epic-bike-react/precache-manifest.0966ff48fd233de9d8539921ca6247c9.js"
 );
 
 self.addEventListener('message', (event) => {
