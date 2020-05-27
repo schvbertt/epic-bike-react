050ec187bf6773c1a928259c2df33c495ca1cd2e
Aleksandra Macura <amacura@biuro.elbis-agdrtv.pl>
2020-05-27 15:44:21 +0200

    Quick fixes

--- src/components/Navi.js
+++ src/components/Navi.js
@@ -8,9 +8,17 @@ import Logo from '../img/logo.png'
 import Texture from '../img/texture.png'
 
 // ICONS
-import {FaBars} from 'react-icons/fa'
+// import {FaBars} from 'react-icons/fa'
 
 export default function Navi() {
+    const hamburgerClose = () => {
+        const hamburgerIcon = document.querySelector('.hamburger')
+        if (hamburgerIcon.classList.contains('is-active') === true) {
+            hamburgerIcon.classList.remove('is-active')
+        } else {
+            hamburgerIcon.classList.add('is-active')
+        }
+    }
     return (
         <>
         <Navbar expand='xl' collapseOnSelect={true} fixed='top' className='animatedDownNavbar fadeInDownNavbar'>
@@ -23,13 +31,20 @@ export default function Navi() {
                         <img src={Texture} className='logo-img' alt='texture'/>
                         <h3 className='logo-img-text'>EPIC LIFE</h3>
                     </div>
-            <Navbar.Toggle aria-controls='navbar'>
-                <FaBars className='fa-bars' size={30} />
+            <Navbar.Toggle onClick={hamburgerClose} aria-controls='navbar'>
+                {/* <FaBars className='fa-bars' size={30} /> */}
+                {/* HAMBURGER */}
+                <div class="hamburger hamburger--3dxy">
+                    <div class="hamburger-box">
+                    <div class="hamburger-inner">
+                    </div>
+                    </div>
+                </div>
             </Navbar.Toggle>
             <Navbar.Collapse id='navbar'>
                 <Nav className='text-center align-items-center ml-auto align-menu'>
                     <LinkContainer exact to='/' activeClassName='active-item'>
-                        <Nav.Link 
+                        <Nav.Link onClick={hamburgerClose}
                         className='d-flex align-items-center justify-content-center link-fix
                         mb-xl-0 mb-lg-3 mb-md-3 mb-sm-3 mb-3 
                         mt-xl-0 mt-lg-3 mt-md-3 mt-sm-3 mt-4'>
@@ -39,7 +54,7 @@ export default function Navi() {
                     </LinkContainer>
 
                     <LinkContainer exact to='/cennik' activeClassName='active-item'>
-                        <Nav.Link 
+                        <Nav.Link onClick={hamburgerClose}
                         className='d-flex align-items-center justify-content-center link-fix
                         mb-xl-0 mb-lg-3 mb-md-3 mb-sm-3 mb-3'>
                             <span className='menu-font'>CENNIK</span>
@@ -48,7 +63,7 @@ export default function Navi() {
                     </LinkContainer>
 
                     <LinkContainer exact to='/kontakt' activeClassName='active-item'>
-                        <Nav.Link 
+                        <Nav.Link onClick={hamburgerClose}
                         className='d-flex align-items-center justify-content-center
                         mb-xl-0 mb-lg-3 mb-md-3 mb-sm-3 mb-3'>
                             <span className='menu-font'>KONTAKT</span>
